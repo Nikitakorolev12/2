1b42f9bff8a815ae725c2b5957c757cb18cf4aaf
vr1men <svrmen12@gmail.com>
2024-10-24 19:28:47 +0300

    Добавьте файлы проекта.

---
+++ UserControlWeather.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppPR2.DataModel;
+
+namespace WpfAppPR2
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControlWeather.xaml
+    /// </summary>
+    public partial class UserControlWeather : UserControl
+    {
+        public UserControlWeather()
+        {
+            InitializeComponent();
+        }
+
+        public UserControlWeather(Weather weather)
+        {
+            InitializeComponent();
+            System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["weatherViewSource"];
+            myCollectionViewSource.Source = new List<Weather>() { weather };
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            // Не загружайте свои данные во время разработки.
+            // if (!System.ComponentModel.DesignerProperties.GetIsInDesignMode(this))
+            // {
+            // 	//Загрузите свои данные здесь и присвойте результат CollectionViewSource.
+            // 	System.Windows.Data.CollectionViewSource myCollectionViewSource = (System.Windows.Data.CollectionViewSource)this.Resources["Resource Key for CollectionViewSource"];
+            // 	myCollectionViewSource.Source = your data
+            // }
+        }
+    }
+}
